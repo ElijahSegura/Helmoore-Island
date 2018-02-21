1f1a9778c9ce127af638257fc6ac22406ca077ef
Elijah Segura <elijahsegurar@gmail.com>
2018-02-20 17:55:18 -0700

    Purge

--- assets/Scripts/Billboard.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Billboard : MonoBehaviour {
-	void Update () {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * -Vector3.forward, Camera.main.transform.rotation * Vector3.up);
-    }
-}
