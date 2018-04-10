ff89d7b54ff5deb1d03afd4f42dfe04939a5f3f0
Xapher <schreckengostb1@gmail.com>
2018-04-09 20:43:14 -0700

    Fixed character map

---
+++ assets/Scripts/Server.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour {
+    NetworkManager nm;
+    private HostData[] hostList;
+    void Start()
+    {
+        nm = GetComponent<NetworkManager>();
+        nm.StartHost();
+        nm.StartClient();
+    }
+}
